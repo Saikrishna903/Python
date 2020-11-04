7e3c2924927f99d119f88cfad240c2dbbf11c47d
Saikrishna903 <73385942+Saikrishna903@users.noreply.github.com>
2020-11-04 22:20:31 +0530

    Second commit

---
+++ temperature.py
@@ -0,0 +1,12 @@
+#Calling an api using requests library.
+import requests
+import json
+location = input("Enter your location to find temperature: ")
+url = "http://api.openweathermap.org/data/2.5/find?appid=d12cf8c5c3c40febafd1dca5750f7eb1&units=metric&q=" + location
+response = requests.get(url)
+if response.status_code == 200:
+	weather_info = response.json()
+	temp_from_weather_info = weather_info['list'][0]['main']['temp']
+	print("Temperature is : " + str (temp_from_weather_info) + chr(176) + "C")
+else:
+	print("RESPONSE NOT FOUND")
